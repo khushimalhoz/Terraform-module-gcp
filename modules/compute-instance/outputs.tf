08e554b6ab25b099f2cced89c2f107c1460247fe
khushimalhoz <khushi.malhotra@mygurukulam.co>
2025-01-22 00:39:56 +0530

    removed compute

--- modules/compute-instance/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_names" {
-  description = "Names of the created instances"
-  value       = google_compute_instance.vm_instance[*].name
-}
-
-output "instance_ips" {
-  description = "Public IPs of the created instances"
-  value       = google_compute_instance.vm_instance[*].network_interface[0].access_config[0].nat_ip
-}
--- modules/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_names" {
-  description = "Names of the created instances"
-  value       = module.compute_instance.instance_names
-}
-
-output "instance_ips" {
-  description = "Public IPs of the created instances"
-  value       = module.compute_instance.instance_ips
-}
